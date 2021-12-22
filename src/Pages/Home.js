516bed935e97bad9a9fdaeeeef4e5e2307647e1b
fred-dev22 <73308599+fred-dev22@users.noreply.github.com>
2021-12-22 17:25:37 +0100

    footer component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
 import { Slogan } from '../components/home-Components/Slogan'
+import Footer from '../components/Footer'
 
 export const Home =()=>{
     return(
         <>
         <Navbar/>
         <Slogan/>
+        <Footer/>
         </>
     )
 }
