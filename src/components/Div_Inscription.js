f55c6a0730aa81da16044a7f59e7475deec85da3
ngwanodimitri <ngwanodimitri@gmail.com>
2021-12-22 17:43:52 +0100

    cc

--- src/components/Div_Inscription.js
+++ src/components/Div_Inscription.js
@@ -4,6 +4,9 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 const Div_Inscriptions = styled.div`
     
@@ -61,8 +64,9 @@ export const Div_Inscription = () => {
                             <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridEmail">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Control type="email" placeholder="Enter email"  />
                                 </Form.Group>
+
 
                                 <Form.Group as={Col} controlId="formGridPassword">
                                     <Form.Label>Password</Form.Label>
@@ -82,7 +86,7 @@ export const Div_Inscription = () => {
 
                             <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridCity">
-                                    <Form.Label>City</Form.Label>
+                                    <Form.Label>Numero CNI</Form.Label>
                                     <Form.Control />
                                 </Form.Group>
 
