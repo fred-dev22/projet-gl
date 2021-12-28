aa5c832c65f4016d12fd12cdd674d3428abc2587
fred-dev22 <73308599+fred-dev22@users.noreply.github.com>
2021-12-28 07:47:43 +0100

    navBar modification

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -180,17 +180,31 @@ background-color:#6c63ff;
         text-align: center;
     }
     .buttonStyle{
-      margin-right:30px;
-      border:solid white 3px ;
-      border-radius:10px;
-      padding:10px;
-      font-size:15px;
-      font-weight:bold;
-      background-color:transparent;
+    margin-right:30px;
+    border:solid white 3px ;
+    border-radius:10px;
+    padding:10px;
+    font-size:15px;
+    font-weight:bold;
+    background-color:transparent;
+    color:white;
+    :hover{
+      background-color:#551a8b;
       color:white;
-      
     }
-  
-  
-}
+  }
+
+  .active-button-link{
+    text-decoration:none;
+    margin-right:30px;
+    border:solid white 3px ;
+    border-radius:10px;
+    padding:10px;
+    font-size:15px;
+    font-weight:bold;
+    background-color:white;
+    color:black;
+  }
+ 
+  }
 `
